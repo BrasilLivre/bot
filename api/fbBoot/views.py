df21949eb2f4181dca3c42ab9815c2d9afb2f609
bruno messias <messias.physics@gmail.com>
2016-09-06 23:58:19 -0300

    Importing modules required for communication with the bot

--- api/fbBoot/views.py
+++ api/fbBoot/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import json, requests, random, re
+from django.views import generic
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 # Create your views here.
