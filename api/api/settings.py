1c50d7df9bf07e4dd2b268a7b341d8513d46b451
bruno_messias <messias.physics@gmail.com>
2016-09-09 00:05:16 -0300

    Add secret data to secret.py
     For obvious security reasons secret.py should not be sent to the repository# Please enter the commit message for your changes. Lines starting

--- api/api/settings.py
+++ api/api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+from secret.py import HashKey
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tdfdee+vyeav64piwr9jezj-st9es)=#+=ba*&#^8k@sl7sz^('
+SECRET_KEY = HashKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- api/fbBoot/views.py
+++ api/fbBoot/views.py
@@ -4,15 +4,14 @@ from django.views import generic
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from ../api/secret.py import PAGE_ACCESS_TOKEN,VERIFY_TOKEN
 
-PAGE_ACCESS_TOKEN = ""
-VERIFY_TOKEN = ""
 
-jokes = { 'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""", 
-                     """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""], 
-         'fat':      ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""", 
-                      """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """], 
-         'dumb': ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""", 
+jokes = { 'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
+                     """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
+         'fat':      ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""",
+                      """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """],
+         'dumb': ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""",
                   """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""] }
 
 # Helper function
@@ -25,13 +24,13 @@ def post_facebook_message(fbid, recevied_message):
             joke_text = random.choice(jokes[token])
             break
     if not joke_text:
-        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!" 
+        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
 
-    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
-    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
-    user_details = requests.get(user_details_url, user_details_params).json() 
+    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
+    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
+    user_details = requests.get(user_details_url, user_details_params).json()
     joke_text = 'Yo '+user_details['first_name']+'..! ' + joke_text
-                   
+
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
@@ -44,7 +43,7 @@ class YoMamaBotView(generic.View):
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
-        
+
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
@@ -58,11 +57,11 @@ class YoMamaBotView(generic.View):
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
                 # Check to make sure the received call is a message call
-                # This might be delivery, optin, postback for other events 
+                # This might be delivery, optin, postback for other events
                 if 'message' in message:
                     # Print the message to the terminal
-                    pprint(message)    
+                    pprint(message)
                     # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
-                    # are sent as attachments and must be handled accordingly. 
-                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+                    # are sent as attachments and must be handled accordingly.
+                    post_facebook_message(message['sender']['id'], message['message']['text'])
         return HttpResponse()
