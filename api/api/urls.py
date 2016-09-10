1c2a2920153003872a26512f68bd1d00cff4e764
bruno_messias <messias.physics@gmail.com>
2016-09-10 20:36:29 -0300

    Fix typing mistake

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^fbBoot/', include('fbBot.urls')),
+    url(r'^fbBot/', include('fbBot.urls')),
 
 ]
