910bb6d26060f4791b46a8e26f64e530fe9e635b
bruno_messias <messias.physics@gmail.com>
2016-09-10 19:56:42 -0300

    Fix stupid mistakes
    I wrote Boot instead of Bot

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^fbBoot/', include('fbBoot.urls')),
+    url(r'^fbBoot/', include('fbBot.urls')),
 
 ]
